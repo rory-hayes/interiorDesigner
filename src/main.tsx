a69f620b1fb71c72fb6b20dcc08ab161a9bbb85b
rory-hayes <54278439+rory-hayes@users.noreply.github.com>
2026-06-21 09:53:33 +0100

    Load customer app shell styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { installRoomPhotoFetchNormalizer } from "./lib/roomPhotoFetchNormalizer";
 import "./styles.css";
+import "./appShell.css";
 
 installRoomPhotoFetchNormalizer();
 
