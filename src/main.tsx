6d5503bfcd920ccd4ea1fb4cde3e459c7ff4dd91
rory-hayes <54278439+rory-hayes@users.noreply.github.com>
2026-06-21 09:35:49 +0100

    Install room photo fetch normalizer

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { installRoomPhotoFetchNormalizer } from "./lib/roomPhotoFetchNormalizer";
 import "./styles.css";
+
+installRoomPhotoFetchNormalizer();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
